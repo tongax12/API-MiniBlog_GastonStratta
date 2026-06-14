26c48c2138d19114ba8f762c1df4a7941f2ac2a6
tongax12 <gastonstratta08@gmail.com>
2026-06-13 22:53:10 -0300

    feat: modularizacion del CRUD en rutas de autores y de posts

--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -13,7 +13,70 @@ router.get('/', async (req,res)=>{
         const authorList = await pool.query('SELECT * FROM authors');
         res.json(result.rows);
     } catch (error) { 
-        console.error('')
+    res.status(500).json( { error: error.message });
     }
 
+});
+
+router.get('/:id', async (req,res) =>{
+    try{
+        const id = Number(req.params.id);
+
+        const result = await pool.query('SELECT * FROM authors WHERE id = $1',[id]);
+
+        if(result.rows.length === 0){ 
+            return res.status(404).json( { error: "Author not found" });
+        }
+
+        res.json(result.rows[0]);
+    } catch(error){
+        res.status(500).json( { error: error.message });
+    }
+});
+
+
+router.post('/', async (req,res) =>{
+    try{
+        const { name, email, bio } = req.body;
+
+        const result = await pool.query(
+            'INSERT INTO authors (name, email, bio) VALUES ($1, $2, $3) RETURNING *',[name,email,bio]
+        )
+
+        res.status(201).json(result.rows[0]);
+    } catch(error) {
+        res.status(500).json( { error: error.messsage})
+    }
+
+});
+
+router.put('/:id', async (req,res) =>{
+    const id = Number(req.params.id);
+    
+    const {name, email, bio};
+    //hacer el if
+    try{
+        const updateAuthor = await pool.query('UPDATE authors name= $1, email= $2, bio= $3 WHERE id = $4 RETURNING *', [name, email,bio, id]);
+        if(updateAuthor.rows.length === 0){
+            return res.status(404).json( {error: "Author not found"} );
+        }
+        res.status(200).json
+    } catch(error) {
+        res.status(500).json( { error: error.messsage})
+    }
+});
+
+router.delete('/:id', async(req,res) => {
+    const id = Number(req.params.id);
+    try{
+    const authorDelete = await pool.query('DELETE FROM authors WHERE id= $1 RETURNING *', [id]);
+    if(authorDelete.rows.length === 0){
+        return res.status(404).json( { error: "Author not found"} );
+    } 
+    res.status(200).json(authorDelete.rows[0]);
+    } catch(error) {
+        res.status(500).json( { error: error.messsage})
+    }
 })
+
+module.exports = router;
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -4,3 +4,128 @@ loadEnvFile('.env');
 const express = require('express');
 const router = express.Router();
 import { createError } from '../../errors';
+
+router.get('/', async (req,res) =>{
+    try{
+        const listPosts = await pool.query('SELECT * FROM posts');
+        res.json(listPosts.rows);
+    } catch(error) {
+        res.status(500).json( { error: error.messsage})
+    }
+})
+
+router.get('/:id', async (req,res) =>{
+    const id = Number(req.params.id);
+    try{
+
+        const post = await pool.query('SELECT * FROM authors WHERE id = $1',[id]);
+
+        if(post.rows.length === 0){ 
+            return res.status(404).json( { error: "Post not found" });
+        }
+
+        res.json(post.rows[0]);
+
+    } catch(error){
+        res.status(500).json( { error: error.message });
+    }
+});
+
+router.get('/author/:id', async (req,res) =>{
+    const authorId  = Number(req.params.id);
+
+    const query = `
+    SELECT
+    posts.id,
+    posts.title,
+    posts.content,
+    posts.published,
+    posts.created_at,
+    authors.id AS author_id,
+    authors.name AS author_name,
+    authors.email AS author_email,
+    authors.bio AS authors_bio
+    FROM posts
+    JOIN authors ON posts.author_id = authors.id
+    WHERE posts.author_id = $1;
+    `;
+
+    try{
+        
+
+        const { rows } = await pool.query( query ,[authorId]);
+
+        if(rows.length === 0){ 
+            return res.status(404).json( { error: "Posts of this author not found" });
+        }
+        
+        const authorAndPost ={
+            author: {
+                id: rows[0].author_id,
+                name: rows[0].author_name,
+                mail: rows[0].author_email,
+                bio: rows[0].author_bio,
+            },
+            posts: rows.map(row => ({
+                id: row.id,
+                title: row.title,
+                content: row.content,
+                published: row.published,
+                created_at: row.created_at,
+            })),
+        };
+
+        res.json(authorAndPost);
+        
+    } catch(error){
+        res.status(500).json( { error: error.message });
+    }
+});
+
+router.post('/', async (req,res) =>{
+   
+    const { title, content, author_id, published } = req.body;
+   
+    try{
+
+        const result = await pool.query(
+            'INSERT INTO posts (title, content, author_id, published) VALUES ($1, $2, $3, $4) RETURNING *',[title, content, author_id, published],
+        )
+
+        res.status(201).json(result.rows[0]);
+    } catch(error) {
+        res.status(500).json( { error: error.messsage})
+    }
+
+});
+
+router.put('/:id', async (req,res) =>{
+    const id = Number(req.params.id);
+    
+    const {title, content, published};
+    //hacer el if
+    try{
+        const updatePost = await pool.query('UPDATE posts title= $1, content= $2, published= $3 WHERE id = $4 RETURNING *', [title, content, published, id]);
+        if(updatePost.rows.length === 0){
+            return res.status(404).json( {error: "Author not found"} );
+        }
+        res.status(200).json
+    } catch(error) {
+        res.status(500).json( { error: error.messsage})
+    }
+});
+
+router.delete('/:id', async(req,res) => {
+    const id = Number(req.params.id);
+    try{
+    const postDelete = await pool.query('DELETE FROM posts WHERE id= $1 RETURNING *', [id]);
+    if(postDelete.rows.length === 0){
+        return res.status(404).json( { error: "Post not found"} );
+    } 
+    res.status(200).json(postDelete.rows[0]);
+    } catch(error) {
+        res.status(500).json( { error: error.messsage})
+    }
+})
+
+module.exports = router;
