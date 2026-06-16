08e0698b4057da3515da1e7dd3b947aaf2072cfb
tongax12 <gastonstratta08@gmail.com>
2026-06-16 20:35:35 -0300

    docs: se agrega la documentación openAPI

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const app = express();
 const authorsRouter = require('./src/routes/authors.routes');
 const postsRouter = require('./src/routes/posts.routes');
 const {errorHandler} = require('./src/middlewares/errorHandler')
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDoc = YAML.load('./openapi.yaml');
 
 
 app.use(express.json());
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
 app.use('/authors', authorsRouter);
+
 app.use('/posts', postsRouter);
 
 app.use(errorHandler);
