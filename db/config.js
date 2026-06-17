2085e631f96181c84e01cb27a8c3610b9b13ddde
tongax12 <gastonstratta08@gmail.com>
2026-06-17 09:45:12 -0300

    chore: cambio en la conexión del pool para poder conectarse a railway

--- db/config.js
+++ db/config.js
@@ -1,12 +1,20 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-});
+const isProduction = process.env.NODE_ENV === 'production';
 
-module.exports = pool;  
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+      }
+);
+
+module.exports = pool; 
